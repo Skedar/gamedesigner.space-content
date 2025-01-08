305e4810f26051ca492a93aaebc1c0cbdd6a9191
Luis "Skedar" Rizzi <Skedar@users.noreply.github.com>
2025-01-08 15:12:35 -0300

    a

--- content/config.ts
+++ content/config.ts
@@ -1,18 +1,34 @@
-import { defineCollection, z } from 'astro:content';
+import { SITE } from "@config";
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDatetime: z.date(),
-    author: z.string(),
-    tags: z.array(z.string()),
-    ogImage: z.string().optional(),
-    featured: z.boolean().optional(),
-  }),
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
+      title: z.string(),
+      featured: z.boolean().optional(),
+      draft: z.boolean().optional(),
+      tags: z.array(z.string()).default(["others"]),
+      ogImage: image()
+        .refine(img => img.width >= 1200 && img.height >= 630, {
+          message: "OpenGraph image must be at least 1200 X 630 pixels!",
+        })
+        .or(z.string())
+        .optional(),
+      description: z.string(),
+      canonicalURL: z.string().optional(),
+      editPost: z
+        .object({
+          disabled: z.boolean().optional(),
+          url: z.string().optional(),
+          text: z.string().optional(),
+          appendFilePath: z.boolean().optional(),
+        })
+        .optional(),
+    }),
 });
 
-export const collections = {
-  'blog': blog,
-};
+export const collections = { blog };
